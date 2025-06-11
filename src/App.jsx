efe226d139b2e8dd32e3cd1a3d7b62d05305ce87
Urvee-Gaikwad <gaikwadurvee1258@gmail.com>
2025-06-11 13:47:02 +0530

    first5 commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,21 +5,30 @@ import AboutPage from "./pages/AboutPage";
 import CoursesPage from "./pages/CoursesPage";
 import ContactPage from "./pages/ContactPage";
 import AdmissionsPage from "./pages/AdmissionsPage";
+import NotFoundPage from "./pages/NotFoundPage";
+import Footer from "./components/Footer/Footer";
+import ChatbotComponent from "./components/Chatbot/ChatbotComponents";
 
 
 const App = () => {
   return (
     <>
       <Router>
-        <Routes>
-          <Route path="/homepage" element={<HomePage/>}/>
-          <Route path="/aboutpage" element={<AboutPage/>}/>
-          <Route path="/coursespage" element={<CoursesPage/>}/>
-          <Route path="/contactpage" element={<ContactPage/>}/>
-          <Route path="/admissionspage" element={<AdmissionsPage/>}/>
-        </Routes>
+        <div className="main-layout">
+          <div className="content">
+          <Routes>
+            <Route path="/homepage" element={<HomePage/>}/>
+            <Route path="/aboutpage" element={<AboutPage/>}/>
+            <Route path="/coursespage" element={<CoursesPage/>}/>
+            <Route path="/contactpage" element={<ContactPage/>}/>
+            <Route path="/admissionspage" element={<AdmissionsPage/>}/>
+            <Route path="*" element={<NotFoundPage/>}/>
+          </Routes>
+          </div>
+          <ChatbotComponent/>
+        </div>
       </Router>
-    </>
+    </>  
   )
 }
 
